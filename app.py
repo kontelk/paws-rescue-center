74ecd73d81d5bd97952af3e35d40090db17e86aa
Konstantinos Telkis <ktelkis@gmail.com>
2025-02-08 13:36:40 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,19 @@
+"""Flask Application for Paws Rescue Center."""
 from flask import Flask, render_template
 app = Flask(__name__)
 
+"""1. Add a View Function for the Home page."""
 @app.route("/")
 def home():
     return "Paws Rescue Center 🐾"
 
+"""2. Add a View Function for the About page."""
 @app.route("/about")
 def learn():
     about_text = """ We are a non-profit organization working as an animal rescue. We aim to help you connect with the purrfect furbaby for you! The animals you find on our website are rescued and rehabilitated animals. Our mission is to promote the ideology "adopt, don't hop"! """
     return about_text
 
+
 if __name__ == "__main__":
+    """ """
     app.run(debug=True, host="0.0.0.0", port=3000)
